792f0a816a034f2acd52061b86223b7d1c9d8b91
FlechaMaker <6488324+FlechaMaker@users.noreply.github.com>
2021-03-17 09:45:39 +0900

    add blue court

--- docs/main.js
+++ docs/main.js
@@ -1,46 +1,64 @@
-let hanamikyaku = []
-hanamikyaku.push(document.getElementById("A-1"))
-hanamikyaku.push(document.getElementById("A-2"))
-hanamikyaku.push(document.getElementById("A-3"))
-hanamikyaku.push(document.getElementById("B-1"))
-hanamikyaku.push(document.getElementById("B-2"))
-hanamikyaku.push(document.getElementById("B-3"))
-hanamikyaku.push(document.getElementById("B-4"))
-hanamikyaku.push(document.getElementById("C-1"))
-hanamikyaku.push(document.getElementById("C-2"))
-hanamikyaku.push(document.getElementById("C-3"))
+let hanamikyakuRed = []
+redCourt = document.getElementById("red-court")
+hanamikyakuRed.push(redCourt.querySelector("#A-1"))
+hanamikyakuRed.push(redCourt.querySelector("#A-2"))
+hanamikyakuRed.push(redCourt.querySelector("#A-3"))
+hanamikyakuRed.push(redCourt.querySelector("#B-1"))
+hanamikyakuRed.push(redCourt.querySelector("#B-2"))
+hanamikyakuRed.push(redCourt.querySelector("#B-3"))
+hanamikyakuRed.push(redCourt.querySelector("#B-4"))
+hanamikyakuRed.push(redCourt.querySelector("#C-1"))
+hanamikyakuRed.push(redCourt.querySelector("#C-2"))
+hanamikyakuRed.push(redCourt.querySelector("#C-3"))
+let hanamikyakuBlue = []
+blueCourt = document.getElementById("blue-court")
+hanamikyakuBlue.push(blueCourt.querySelector("#A-1"))
+hanamikyakuBlue.push(blueCourt.querySelector("#A-2"))
+hanamikyakuBlue.push(blueCourt.querySelector("#A-3"))
+hanamikyakuBlue.push(blueCourt.querySelector("#B-1"))
+hanamikyakuBlue.push(blueCourt.querySelector("#B-2"))
+hanamikyakuBlue.push(blueCourt.querySelector("#B-3"))
+hanamikyakuBlue.push(blueCourt.querySelector("#B-4"))
+hanamikyakuBlue.push(blueCourt.querySelector("#C-1"))
+hanamikyakuBlue.push(blueCourt.querySelector("#C-2"))
+hanamikyakuBlue.push(blueCourt.querySelector("#C-3"))
 const CARDBOARD_COLOR = "rgb(203, 169, 123)"
 const DEFAULT_COLOR = "limegreen"
 const KYAKU_N = 6
 const POSITION_N = 10
 
 let isFilled = (position) => {
-    if (hanamikyaku[position].style.backgroundColor == CARDBOARD_COLOR)
+    if (hanamikyakuRed[position].style.backgroundColor == CARDBOARD_COLOR)
         return true
 
     if (position < 3) {
-        let count = (hanamikyaku[0].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[1].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[2].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
+        let count = (hanamikyakuRed[0].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[1].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[2].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
         return count >= 2
     } else if (position >= 3 && position < 7) {
-        let count = (hanamikyaku[3].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[4].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[5].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[6].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
+        let count = (hanamikyakuRed[3].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[4].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[5].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[6].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
         return count >= 3
     } else {
-        let count = (hanamikyaku[7].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[8].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyaku[9].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
+        let count = (hanamikyakuRed[7].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[8].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0) + (hanamikyakuRed[9].style.backgroundColor == CARDBOARD_COLOR ? 1 : 0)
         return count >= 2
     }
 }
 
 document.getElementById("lottery").addEventListener("click", () => {
-    hanamikyaku.forEach((h) => {
+    hanamikyakuRed.forEach((h) => {
         h.style.backgroundColor = DEFAULT_COLOR
     })
+    hanamikyakuBlue.forEach((h) => {
+        h.style.backgroundColor = DEFAULT_COLOR
+    })
+
 
     let n = 0;
     while (n < KYAKU_N) {
         let r = Math.floor(Math.random() * POSITION_N)
         console.log(`r:${r} n:${n}`)
         if (!isFilled(r)) {
-            hanamikyaku[r].style.backgroundColor = CARDBOARD_COLOR
+            hanamikyakuRed[r].style.backgroundColor = CARDBOARD_COLOR
+            hanamikyakuBlue[r].style.backgroundColor = CARDBOARD_COLOR
             n++
         }
     }
